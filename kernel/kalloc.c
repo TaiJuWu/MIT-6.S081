36068cd465603f07ad6eb8762eb2a5ba5ff7e6b1
tjwu <tjwu1217@gmail.com>
2023-03-22 11:10:28 +0800

    feature: Finish Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void *get_phyical_mem(int cpu_id);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -19,14 +20,16 @@ struct run {
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+  struct spinlock lock[NCPU];
+  struct run *freelist[NCPU];
 } kmem;
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; ++i){
+    initlock(&kmem.lock[i], "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -47,6 +50,11 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int cpu_id;
+
+  push_off();
+  cpu_id = cpuid();
+  pop_off();
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +64,10 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem.lock[cpu_id]);
+  r->next = kmem.freelist[cpu_id];
+  kmem.freelist[cpu_id] = r;
+  release(&kmem.lock[cpu_id]);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,14 +77,35 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int cpu_id;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  cpu_id = cpuid();
+  pop_off();
+
+  r = get_phyical_mem(cpu_id);
+
+  // steal memory from other cpu's freelist
+  for(int i = 0; i < NCPU && !r; ++i) {
+    if(cpu_id == i) continue;
+    r = get_phyical_mem(i);
+  }
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+void *
+get_phyical_mem(int cpu_id)
+{
+  struct run *r;
+
+  acquire(&kmem.lock[cpu_id]);
+    r = kmem.freelist[cpu_id];
+    if(r)
+      kmem.freelist[cpu_id] = r->next;
+    release(&kmem.lock[cpu_id]);
+
+  return (void *)r;
 }
